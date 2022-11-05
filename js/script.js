dd98f44530e3e99f81b938d503476338c7c08855
Ricardo Tadei Romero <cl201605@g.unicamp.br>
2022-11-05 17:34:31 -0300

    Upando

---
+++ js/script.js
@@ -0,0 +1,14 @@
+ function SaibaMais(id, id2) {
+    var button = document.getElementById(id2);
+
+    let card = document.getElementById(id);
+    card.classList.toggle('active')
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button.textContent = 'Saiba Menos';
+    }
+    
+    // Change button text if hasn't class active
+    button.textContent = 'Saiba Mais';
+};
